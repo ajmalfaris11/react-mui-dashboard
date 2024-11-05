1aa1b80ff3b8594a91af9ce3e2ea9451110e5f94
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-05 18:48:30 +0530

    handleSubmit function create and navigate to the index file ('/')

--- src/app/pages/Signin/index.js
+++ src/app/pages/Signin/index.js
@@ -1,72 +1,81 @@
-import { Box, Button, TextField, Typography } from '@mui/material';
-import React from 'react'
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SignInPage() {
+  const navigate = useNavigate();
+
+  const handleSumbit = (event) => {
+    event.preventDefault();
+    navigate("/");
+  };
+
   return (
     <Box
-    sx={{
-      height: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    <Box
       sx={{
-        width: 400,
-        textAlign: 'center',
-        padding: 5,
-        backgroundColor: 'white',
-        boxShadow: 3,
-        borderRadius: 2,
-        
-        '& .MuiOutlinedInput-root': {
-          '& fieldset': {
-            borderColor: 'black',
-          },
-          '&:hover fieldset': {
-            borderColor: 'darkgray',
-          },
-          '&.Mui-focused fieldset': {
-            borderColor: 'black',
-          },
-        },
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <Typography variant="h5" sx={{ mb: 3 }}>
-        Sign In
-      </Typography>
-      <TextField
-        label="Email Address"
-        variant="outlined"
-        fullWidth
-        sx={{ mb: 2 }}
-        color='black'
-      />
-      <TextField
-        label="Password"
-        variant="outlined"
-        type="password"
-        fullWidth
-        sx={{ mb: 3 }}
-        color='black'
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        fullWidth
+      <Box
+        component="form"
+        onSubmit={handleSumbit}
         sx={{
-          backgroundColor: 'black',
-          '&:hover': {
-            backgroundColor: '#333',
+          width: 400,
+          textAlign: "center",
+          padding: 5,
+          backgroundColor: "white",
+          boxShadow: 3,
+          borderRadius: 2,
+
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "black",
+            },
+            "&:hover fieldset": {
+              borderColor: "darkgray",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "black",
+            },
           },
         }}
       >
-        SIGN IN
-      </Button>
+        <Typography variant="h5" sx={{ mb: 3 }}>
+          Sign In
+        </Typography>
+        <TextField
+          label="Email Address"
+          variant="outlined"
+          fullWidth
+          sx={{ mb: 2 }}
+          color="black"
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          fullWidth
+          sx={{ mb: 3 }}
+          color="black"
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          sx={{
+            backgroundColor: "black",
+            "&:hover": {
+              backgroundColor: "#333",
+            },
+          }}
+        >
+          SIGN IN
+        </Button>
+      </Box>
     </Box>
-  </Box>
-);
+  );
 }
-
- 
